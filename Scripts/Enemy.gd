057e938c5e9b580eda8c9ed4441b9c222d0e388e
Qufel <97023524+Qufel@users.noreply.github.com>
2024-09-09 09:27:01 +0200

    Enemy movement

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -1,15 +1,28 @@
-extends Node2D
+extends CharacterBody2D
 class_name Enemy
 
 @export var health : float = 10.0
 @export var gold : int = 3
 
-@export var sprite : Sprite2D
-
 @export var movementSpeed : float = 10.0
+@export var acceleration : float = 3.0
 
-func _init():
-	pass
+@onready var nav = $NavigationAgent2D
+@export var end : Node2D
 
-func moveToNextWaypoint(waypoint : Vector2):
-	pass
+func _physics_process(delta):
+	moveToPoint(delta)
+	
+func moveToPoint(delta : float):
+	
+	await get_tree().physics_frame
+	
+	var dir : Vector2 = Vector2()
+	nav.target_position = end.global_position
+	
+	dir = nav.get_next_path_position() - global_position
+	dir = dir.normalized()
+	
+	velocity = velocity.lerp(dir * movementSpeed, acceleration * delta)
+	
+	move_and_slide()
--- Scripts/Waypoint.gd
+++
@@ -1,4 +0,0 @@
-extends Node2D
-class_name Waypoint
-
-@export var reachRadius : float = 1.0 
